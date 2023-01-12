879a2b3bbeb8c34a09a772abe9f75dcc57176d9a
Meraj Faizan <mdmaraj1500@gmail.com>
2023-01-12 20:55:53 +0600

    mui explored

--- src/App.js
+++ src/App.js
@@ -1,44 +1,29 @@
-import { LoadingButton } from "@mui/lab";
-import { Button } from "@mui/material";
-import { useState } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import Layout from "./components/Layout/Layout";
+import Home from "./pages/Home/Home";
+import { theme } from "./theme/theme";
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    path: "/",
+    children: [
+      {
+        element: <Home />,
+        path: "/",
+      },
+    ],
+  },
+]);
 
 function App() {
-  const [loading, setLoading] = useState(false);
-  function handleClick() {
-    setLoading(!loading);
-  }
   return (
-    <div className="App">
-      <h1>hello world, i am meraj faizan.</h1>
-      <Button variant="text" style={{ margin: "10px" }}>
-        Learn More
-      </Button>
-      <Button variant="contained" style={{ margin: "10px" }}>
-        Learn More
-      </Button>
-      <Button variant="outlined" style={{ margin: "10px" }}>
-        Learn More
-      </Button>
-      <LoadingButton
-        size="small"
-        onClick={handleClick}
-        loading={loading}
-        loadingIndicator="Loading…"
-        variant="outlined"
-      >
-        Fetch data
-      </LoadingButton>
-      <LoadingButton
-        color="secondary"
-        onClick={handleClick}
-        loading={loading}
-        loadingPosition="start"
-        variant="contained"
-      >
-        Save
-      </LoadingButton>
-    </div>
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+      <CssBaseline />
+    </ThemeProvider>
   );
 }
 
